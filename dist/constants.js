c64d141d1d269933288b15895d862742390ca8e7
ZhangZisu <admin@zhangzisu.cn>
2018-11-29 23:50:31 +0800

    Remove dist

--- dist/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tmp_1 = require("tmp");
-const tmp = tmp_1.dirSync();
-exports.tmpDir = tmp.name;
-//# sourceMappingURL=constants.js.map
