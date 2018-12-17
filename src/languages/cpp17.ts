e3246a192f22246f133c10025ee32deefa7cb7dc
ZhangZisu <admin@zhangzisu.cn>
2018-12-17 20:06:29 +0800

    Support new language

---
+++ src/languages/cpp17.ts
@@ -0,0 +1,21 @@
+import { ILanguage } from "../interface";
+
+const cpp17: ILanguage = {
+    compile: {
+        executable: "/usr/bin/env",
+        memoryLimit: 512 * 1024,
+        timeLimit: 3,
+        processLimit: 5,
+        arguments: ["g++", "main.cpp", "-o", "main", "-Wall", "-O2", "-std=c++17"],
+        sourceFileName: "main.cpp",
+        distFileName: "main",
+    },
+    run: {
+        executable: "main",
+        filename: "main",
+        arguments: [],
+        processLimit: 1,
+    },
+};
+
+module.exports = cpp17;
